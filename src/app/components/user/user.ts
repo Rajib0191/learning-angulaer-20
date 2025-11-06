2d7865b1ac54ca6b096283deb59c9561ad39e987
rajib <rajibislam700@gmail.com>
2025-11-06 16:14:27 +0600

    complete reactive form control

--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,26 +1,58 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User implements OnInit, AfterViewInit, OnDestroy {
-  // Execute After Component Initialize.
-  ngOnInit(): void {
-    console.log('Run ngOnInit! - LifeCycle!');
-    // api call
-    // subscription
+export class User {
+  userForm: FormGroup = new FormGroup({
+    userId: new FormControl(0),
+    emailId: new FormControl(''),
+    password: new FormControl(''),
+    fullName: new FormControl(''),
+    mobileNo: new FormControl(''),
+  });
+
+  http = inject(HttpClient);
+  userList$: Observable<any[]>;
+
+  constructor() {
+    this.userList$ = this.http.get<any[]>(
+      'https://api.freeprojectapi.com/api/GoalTracker/getAllUsers'
+    );
   }
 
-  // Execute After All Component/View Initialize
-  ngAfterViewInit(): void {
-    console.log('Run ngAfterViewInit! - LifeCycle!');
+  onClear() {
+    this.userForm.reset();
   }
 
-  // Execute when component destroy, i meant when redirect one page to another page.
-  ngOnDestroy(): void {
-    console.log('ngOnDestroy');
+  onSaveUser() {
+    const formValue = this.userForm.value;
+    this.http.post('https://api.freeprojectapi.com/api/GoalTracker/register', formValue).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
   }
+
+  onEdit(data: any) {
+    this.userForm = new FormGroup({
+      userId: new FormControl(data?.userId),
+      emailId: new FormControl(data?.emailId),
+      password: new FormControl(data?.password),
+      fullName: new FormControl(data?.fullName),
+      mobileNo: new FormControl(data?.mobileNo),
+    });
+  }
+
+  onUpdateUser() {}
 }
